12d41278e08ab2e8a19f378ecbd5156905733f9a
Darwin Monroy <contact@darwinmonroy.com>
2018-04-01 10:50:42 -0600

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup_params = dict(
     name='mettle_protocol',
-    version='1.2.0',
+    version='1.2.1',
     author='Y Team',
     author_email=', '.join([
         'alejandro.rivera@yougov.com',
